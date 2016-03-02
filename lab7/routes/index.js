548b8c8748d3b1124ec570eefb095e4d920d6efb
John <john.robert.larson@gmail.com>
2016-03-02 23:26:20 +0000

    85%

--- lab7/routes/index.js
+++ lab7/routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+var fs = require('fs');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -11,10 +13,21 @@ router.get('/getcity/', function(req, res, next) {
 	var letters = req.query.q;
 	console.log(letters);
 	var callback = req.query.callback;
-	json = '[{"city": "placeholder"}]';
-	var body = callback + '(' + json + ')';
-	res.send(body);
-	console.log(body);
+	fs.readFile('files/cities.dat.txt', function(err, data) {
+		if(err) throw err;
+		var cities = data.toString().split('\n');
+		var re = new RegExp('^' + letters);
+		var filtered = [];
+		for(var i = 0; i < cities.length; i++) {
+			if(re.test(cities[i])) {
+				filtered.push({city: cities[i]});
+			}
+		}
+		json = JSON.stringify(filtered);
+		var body = callback + '(' + json + ')';
+		res.send(body);
+		console.log(body);
+	});
 });
 
 module.exports = router;
