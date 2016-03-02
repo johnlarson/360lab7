f2607fe132e050f89acb9f07314f3cd2ba44d6c9
John <john.robert.larson@gmail.com>
2016-03-02 23:00:18 +0000

    10%

--- lab7/routes/index.js
+++ lab7/routes/index.js
@@ -6,10 +6,15 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/getcity', function(req, res, next) {
+router.get('/getcity/', function(req, res, next) {
+	console.log(req);
 	var letters = req.query.q;
-
-	res.send('[{city: "placeholder"}');
+	console.log(letters);
+	var callback = req.query.callback;
+	json = '[{"city": "placeholder"}]';
+	var body = callback + '(' + json + ')';
+	res.send(body);
+	console.log(body);
 });
 
 module.exports = router;
